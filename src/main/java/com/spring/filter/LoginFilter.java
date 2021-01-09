747d3acf147c2596e2e2b32a4164a2d14af47b40
qw851210@gmail.com <>
2021-01-09 09:57:03 +0800

    SpringData1114

---
+++ src/main/java/com/spring/filter/LoginFilter.java
@@ -0,0 +1,35 @@
+package com.spring.filter;
+
+import com.spring.mvc.psi.repository.UserRepository;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+@Component
+@Order(1)
+@WebFilter("/mvc/*")
+public class LoginFilter extends BaseFilter{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        super.init(); 
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, getFilterConfig().getServletContext());
+    }
+    
+    
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        
+        chain.doFilter(req, res);
+    }
+    
+}
